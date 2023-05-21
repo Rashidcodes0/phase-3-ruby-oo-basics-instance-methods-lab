3410817f624f7315a46274fa4da667389dfdb0c6
Abdirashidnoor <abdirashid16112@gmail.com>
2023-05-21 23:27:34 +0300

    completed

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+# lib/person.rb
+class Person
+end
+
+class Person
+    def talk
+      puts "Hello World!"
+    end
+  end
+
+  class Person
+    def walk
+      puts "The Person is walking"
+    end
+  end
+  
